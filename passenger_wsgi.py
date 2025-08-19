08633d91fab05084129dc6a8f09935998ced84b0
alberic-walsh <albericwdsrt@gmail.com>
2025-08-19 21:31:28 +0200

    catch error

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -9,13 +9,17 @@ def load_app():
     """
     Charge dynamiquement le module FastAPI et retourne l'application ASGI compatible WSGI.
     """
-    if APP_MODULE in sys.modules:
-        # Si le module est déjà importé, on le recharge pour prendre en compte les changements
-        importlib.reload(sys.modules[APP_MODULE])
-    module = importlib.import_module(APP_MODULE)
+    try:
+        if APP_MODULE in sys.modules:
+            # Si le module est déjà importé, on le recharge pour prendre en compte les changements
+            importlib.reload(sys.modules[APP_MODULE])
+        module = importlib.import_module(APP_MODULE)
 
-    # On récupère l'objet FastAPI depuis le module
-    return ASGIMiddleware(module.app)
+        # On récupère l'objet FastAPI depuis le module
+        return ASGIMiddleware(module.app)
+    except ImportError as e:
+        print(f"Erreur lors du chargement du module {APP_MODULE}: {e}")
+        sys.exit(1)
 
 # Variable globale que Passenger va utiliser comme entry point
 application = load_app()
